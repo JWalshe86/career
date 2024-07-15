c979dac11854068906df18a0e181e5f9f3642727
John Walshe <walshejohnnyw7@gmail.com>
2024-07-15 19:47:25 +0100

    :construction: Add job search model

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Jobsearch
+
+
+class JobsearchAdmin(admin.ModelAdmin):
+    list_display = (
+         "first_name",
+         "last_name",
+            )
+
+admin.site.register(Jobsearch)
+
+
 
 # Register your models here.
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Jobsearch(models.Model):
+
+    first_name = models.CharField(max_length=127)
+    last_name = models.CharField(max_length=127)
+    email = models.CharField(max_length=127, null=True, blank=True, default=None)
+    phone = models.CharField(max_length=127, null=True, blank=True, default=None)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
