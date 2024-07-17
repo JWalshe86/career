96ceedcf3647beac25c272d6aba40879dcd37ee3
John Walshe <walshejohnnyw7@gmail.com>
2024-07-17 16:19:34 +0100

    :hammer: Add config to connect to mysql

--- career/settings.py
+++ career/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'career.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_db',
+        'USER': 'root',
+        'PASSWORD': 'Sunshine7!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
