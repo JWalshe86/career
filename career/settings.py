6095aa39811721ab6082fef2fbe862638f8d5c1e
John Walshe <walshejohnnyw7@gmail.com>
2024-07-17 15:39:01 +0100

    :construction: Create model tailored for job searches

--- career/settings.py
+++ career/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
     'jobs',
 ]
 
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'career.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_db',
+        'USER': 'root',
+        'PASSWORD': 'Sunshine7!',
+        'HOST': 'localhost',
+        'PORT': '3306', 
     }
 }
 
--- jobs/models.py
+++ jobs/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 class Jobsearch(models.Model):
 
-    first_name = models.CharField(max_length=127)
-    last_name = models.CharField(max_length=127)
-    email = models.CharField(max_length=127, null=True, blank=True, default=None)
-    phone = models.CharField(max_length=127, null=True, blank=True, default=None)
+    organistion = models.CharField(max_length=127)
+    url = models.URLField(max_length=300)
+    contact = models.CharField(max_length=127, null=True, blank=True, default=None)
+    role = models.CharField(max_length=127, null=True, blank=True, default=None)
+    text_used = models.TextField()
+    method = models.CharField(max_length=127, null=True, blank=True, default=None)
+    response = models.CharField(max_length=127, null=True, blank=True, default=None)
+    search_imgs = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
