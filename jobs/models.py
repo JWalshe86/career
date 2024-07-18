8730fda69b14d8ca07cbc24621d4c285f59c85ea
John Walshe <walshejohnnyw7@gmail.com>
2024-07-18 15:23:25 +0100

    :construction: Add file upload to jobsearch model

--- jobs/models.py
+++ jobs/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 class Jobsearch(models.Model):
 
-    first_name = models.CharField(max_length=127)
-    last_name = models.CharField(max_length=127)
-    email = models.CharField(max_length=127, null=True, blank=True, default=None)
-    phone = models.CharField(max_length=127, null=True, blank=True, default=None)
+    organisation = models.CharField(max_length=127)
+    url = models.URLField(max_length=300)
+    contact = models.CharField(max_length=127, null=True, blank=True, default=None)
+    role = models.CharField(blank=True, default=None, max_length=127, null=True)
+    text_used = models.TextField()
+    method = models.CharField(max_length=127, null=True, blank=True, default=None)
+    response = models.CharField(blank=True, default=None, max_length=127, null=True)
+    search_imgs = models.ImageField(blank=True, upload_to=None)
+    docfile = models.FileField(blank=True, upload_to='documents/%Y/%m/%d')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
