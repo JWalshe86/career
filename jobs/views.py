1e471c8689bcab8fdaf8fa2d58f2c61315bc344f
John Walshe <walshejohnnyw7@gmail.com>
2024-07-20 23:27:12 +0100

    :hammer: Add images render using static

--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Jobsearch
 from .forms import JobsearchForm
 
@@ -11,6 +11,18 @@ def display_data(request):
         "display_data": jobs,
             } 
     return render(request, "jobs/job_searches.html",context)
+
+
+def job_search_detail(request, job_search_id):
+    """A view to show job search details"""
+
+    jobdetail = get_object_or_404(Jobsearch, pk=job_search_id)
+
+    context = {
+        "jobdetail": jobdetail,
+    }
+    return render(request, "jobs/job_search_detail.html", context)
+
 
 
 def add_data(request):
