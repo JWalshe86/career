73f7060cdf80a509d8dcaca6d44633ca42f27314
John Walshe <walshejohnnyw7@gmail.com>
2024-07-26 15:45:54 +0100

    :fire: Remove url & text required add job search model

--- jobs/models.py
+++ jobs/models.py
@@ -2,6 +2,7 @@ from django.db import models
     
 
 METHOD_CHOICES = (
+    ('lkeasy', 'LKEASY'),
     ('lkpsearch', 'LKPSearch'),
     ('cislack', 'CISLACK'),
     ('lkjobsug', 'LKJOBSUG'),
@@ -22,11 +23,11 @@ class Jobsearch(models.Model):
     organisation = models.CharField(max_length=127)
     type = models.CharField(blank=True, choices=TYPE_CHOICES, default='private', max_length=127, null=True)
     location = models.CharField(max_length=127)
-    url = models.URLField(max_length=300, null=True, blank=True)
+    url = models.CharField(max_length=100, null=True, blank=True)
     contact = models.CharField(max_length=127, null=True, blank=True, default=None)
     role = models.CharField(blank=True, default=None, max_length=127, null=True)
-    text_used = models.TextField()
-    method = models.CharField(blank=True, choices=METHOD_CHOICES, default='lkpsearch', max_length=127, null=True)
+    text_used = models.TextField(null=True, blank=True)
+    method = models.CharField(blank=True, choices=METHOD_CHOICES, default='lkeasy', max_length=127, null=True)
     response = models.CharField(max_length=127, null=True, blank=True, default=None)
     search_imgs = models.ImageField(blank=True, upload_to='static/images/%Y/%m/%d')
     docfile = models.FileField(blank=True, upload_to='static/documents/%Y/%m/%d')
