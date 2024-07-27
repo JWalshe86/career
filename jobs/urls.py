cdcb040906d994f66cbb8e2f0db069e0d59b94d8
John Walshe <walshejohnnyw7@gmail.com>
2024-07-27 14:02:12 +0100

    :link: Add url links to navbar

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-          path("display_data", views.display_data, name="display_data"),
-          path("add_data", views.add_data, name="add_data"),
+          path("display_data/", views.display_data, name="display_data"),
+          path("add_data/", views.add_data, name="add_data"),
           path("^<int:jobsearch_id>/$", views.jobsearch_detail, name="jobsearch_detail"),
           path("edit/<int:jobsearch_id>/", views.edit_jobsearch, name="edit_jobsearch"),
         ]
