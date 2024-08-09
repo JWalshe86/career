b0f9efab8557bcdfbec1bf816f38f1dcf1c48556
John Walshe <walshejohnnyw7@gmail.com>
2024-08-09 16:05:24 +0100

    Add whitenoise

--- career/settings.py
+++ career/settings.py
@@ -59,6 +59,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,6 +153,14 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = "~/projects/career/static"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
