1be0efacdec16cf39e4b69f6e4a1ec50b19dc6e6
John Walshe <walshejohnnyw7@gmail.com>
2024-08-18 20:57:16 +0100

    Add form adds task to db

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import *
+from .forms import *
 
 def index(request):
 
     tasks = Task.objects.all()
+    
+    form = TaskForm()
 
-    context = {'tasks': tasks}
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/tasks/')
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
 
 
