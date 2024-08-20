8f3813d425b8f406eb0cb1b8f8098f14da5c0d9e
John Walshe <walshejohnnyw7@gmail.com>
2024-08-20 10:21:21 +0100

    Set default job application status to "pending<wk" in the form #47

--- jobs/forms.py
+++ jobs/forms.py
@@ -8,6 +8,11 @@ class JobsearchForm(forms.ModelForm):
         model = Jobsearch
         fields = "__all__"
 
+    def __init__(self, *args, **kwargs):
+        super(JobsearchForm, self).__init__(*args, **kwargs)
+        if not self.instance.pk:  # New instance, set default value for status
+            self.fields['status'].initial = 'pending<wk'
+
 
 class DateForm(forms.Form):
     start = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
