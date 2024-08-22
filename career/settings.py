86fe18433c484d8bbad3f5ea9b2bb7a8dd30d3d0
John Walshe <walshejohnnyw7@gmail.com>
2024-08-22 22:22:09 +0100

    Configure static files handling for Heroku deployment: Added Whitenoise for static file serving, adjusted STATIC_ROOT and STATICFILES_STORAGE settings, and ensured proper collection of static files.

--- career/settings.py
+++ career/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
