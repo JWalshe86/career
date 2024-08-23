9a5a1226affe0e0d68d4a8cfe0e300316d012192
John Walshe <walshejohnnyw7@gmail.com>
2024-08-23 08:08:42 +0100

    Update tasks view

--- jobs/views.py
+++ jobs/views.py
@@ -14,6 +14,8 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+from tasks.models import Task
 from .models import Jobsearch
 from .forms import JobsearchForm
 
