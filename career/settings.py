ef41d842a317a19b7e983bead67b88e80fee2fd8
John Walshe <walshejohnnyw7@gmail.com>
2024-08-23 15:45:04 +0100

    Edit staticdirs

--- career/settings.py
+++ career/settings.py
@@ -145,11 +145,10 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = "/static/"
-
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Enable gzip and brotli compression for faster load times
 if not DEBUG:
