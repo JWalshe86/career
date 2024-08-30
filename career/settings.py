5693937a42ad99487f3071802534500b4c99c3a5
John Walshe <walshejohnnyw7@gmail.com>
2024-08-30 13:35:05 +0100

    Add whitenoise to middleware

--- career/settings.py
+++ career/settings.py
@@ -238,6 +238,7 @@ WSGI_APPLICATION = 'career.wsgi.application'
 # Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
