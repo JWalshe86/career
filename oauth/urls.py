ae4c5ca095e30d5e8f3b8ae48a50559e40647632
John Walshe <walshejohnnyw7@gmail.com>
2024-09-02 20:08:20 +0100

    Correct urls

--- oauth/urls.py
+++ oauth/urls.py
@@ -1,9 +1,8 @@
-# In your Django app's urls.py (e.g., oauth/urls.py)
+# oauth/urls.py
 from django.urls import path
-from .views import oauth2callback, jobs_dashboard_with_emails
+from .views import oauth2callback
 
 urlpatterns = [
-    path('oauth/jobs-dashboard/', jobs_dashboard_with_emails, name='jobs_dashboard_with_emails'),
-    path('oauth2callback/', oauth2callback, name='oauth2callback'),
+    path('jobs-dashboard/oauth2callback/', oauth2callback, name='oauth2callback'),
 ]
 
