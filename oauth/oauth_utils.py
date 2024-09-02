33e2460a344d7170309c407fda2d977f48c19d89
John Walshe <walshejohnnyw7@gmail.com>
2024-09-02 18:49:58 +0100

    Add iniatate authflow

--- oauth/oauth_utils.py
+++ oauth/oauth_utils.py
@@ -48,6 +48,7 @@ def get_oauth2_authorization_url():
         raise
 
 
+
 def get_unread_emails():
     creds = None
     auth_url = None
@@ -66,10 +67,7 @@ def get_unread_emails():
             creds = Credentials.from_authorized_user_file('token.json', SCOPES)
             logger.debug("Loaded credentials from token.json.")
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
+            creds = initiate_oauth2_flow()
             logger.info("Saved credentials to token.json.")
 
     if creds and creds.expired and creds.refresh_token:
