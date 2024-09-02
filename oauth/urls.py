7485fcabce76a045a04e3fa68abf56c34c0d6b53
John Walshe <walshejohnnyw7@gmail.com>
2024-09-02 20:13:46 +0100

    Correct urls2

--- oauth/urls.py
+++ oauth/urls.py
@@ -1,8 +1,9 @@
 # oauth/urls.py
 from django.urls import path
-from .views import oauth2callback
+from .views import jobs_dashboard_with_emails, oauth2callback
 
 urlpatterns = [
-    path('jobs-dashboard/oauth2callback/', oauth2callback, name='oauth2callback'),
+    path('jobs-dashboard/', jobs_dashboard_with_emails, name='jobs_dashboard_with_emails'),
+    path('oauth2callback/', oauth2callback, name='oauth2callback'),
 ]
 
