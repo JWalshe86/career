9a4f771627c63a29236ebf80a6dbcd3ba3c7dc62
John Walshe <walshejohnnyw7@gmail.com>
2024-09-02 20:42:07 +0100

    Add CSRF_TRUSTED_ORIGINS

--- career/settings.py
+++ career/settings.py
@@ -8,6 +8,9 @@ import dj_database_url
 # Load environment variables from .env file (for local development)
 load_dotenv()
 
+
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='https://www.jwalshedev.ie,https://johnsite.herokuapp.com,https://5b57-86-46-100-229.ngrok-free.app', cast=Csv())
+
 # Set debug mode
 DEBUG = config('DEBUG', default=False, cast=bool)
 ROOT_URLCONF = 'career.urls'  # Adjust according to your project name
