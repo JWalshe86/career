3ff0be9cd8bb297c8943ac240bc1b19e518d1016
John Walshe <walshejohnnyw7@gmail.com>
2024-09-02 19:59:40 +0100

    Change redirect uri in settings.py

--- oauth/urls.py
+++ oauth/urls.py
@@ -1,10 +1,9 @@
-# oauth/urls.py
-
+# In your Django app's urls.py (e.g., oauth/urls.py)
 from django.urls import path
 from .views import oauth2callback, jobs_dashboard_with_emails
 
 urlpatterns = [
+    path('oauth/jobs-dashboard/', jobs_dashboard_with_emails, name='jobs_dashboard_with_emails'),
     path('oauth2callback/', oauth2callback, name='oauth2callback'),
-    path('jobs-dashboard/', jobs_dashboard_with_emails, name='jobs_dashboard_with_emails'),
 ]
 
