9592e96a004d17a1e35f3192b6d27bdc2064b902
John Walshe <walshejohnnyw7@gmail.com>
2024-09-03 22:33:08 +0100

    Update settings.py redirect url

--- career/settings.py
+++ career/settings.py
@@ -17,7 +17,7 @@ ROOT_URLCONF = 'career.urls'
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
 GOOGLE_REDIRECT_URIS = [
-                'http://127.0.0.1:8000/oauth/jobs-dashboard/',
+        'https://www.jwalshedev.ie/oauth/jobs-dashboard/',
                 ]
 
 SCOPES = json.loads(os.getenv('SCOPES', '["https://www.googleapis.com/auth/gmail.readonly"]'))
