54242e5f06e4772e06623a71f911372131c7b473
John Walshe <walshejohnnyw7@gmail.com>
2024-09-03 14:17:25 +0100

    Update callback paths

--- oauth/urls.py
+++ oauth/urls.py
@@ -1,4 +1,3 @@
-# oauth/urls.py
 from django.urls import path
 from .views import jobs_dashboard_with_emails, oauth2callback, env_vars, oauth_login
 
@@ -6,8 +5,7 @@ urlpatterns = [
     # Updated the path for the jobs dashboard with emails view
     path('oauth/jobs-dashboard/', jobs_dashboard_with_emails, name='jobs_dashboard_with_emails'),
     path('oauth/login/', oauth_login, name='oauth_login'),
-    path('env-vars/', env_vars, name='env_vars'),
-    # Updated the path for the OAuth2 callback to reflect the new structure
-    path('jobs-dashboard/oauth2callback/', oauth2callback, name='oauth2callback'),
+    # Update the path for the OAuth2 callback to match the redirect URI
+    path('oauth/jobs-dashboard/', oauth2callback, name='oauth2callback'),
 ]
 
