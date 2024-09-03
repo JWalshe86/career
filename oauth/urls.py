042ff0595e7b62f15a6b13883ea0b2a705a5143a
John Walshe <walshejohnnyw7@gmail.com>
2024-09-03 14:04:28 +0100

    Correct path for oauth

--- oauth/urls.py
+++ oauth/urls.py
@@ -4,7 +4,7 @@ from .views import jobs_dashboard_with_emails, oauth2callback, env_vars, oauth_l
 
 urlpatterns = [
     # Updated the path for the jobs dashboard with emails view
-    path('jobs-dashboard/', jobs_dashboard_with_emails, name='jobs_dashboard_with_emails'),
+    path('oauth/jobs-dashboard/', jobs_dashboard_with_emails, name='jobs_dashboard_with_emails'),
     path('oauth/login/', oauth_login, name='oauth_login'),
     path('env-vars/', env_vars, name='env_vars'),
     # Updated the path for the OAuth2 callback to reflect the new structure
