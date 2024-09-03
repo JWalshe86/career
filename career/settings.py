d0e9af469cbdc53ad812cc102bc6fbd21419e17f
John Walshe <walshejohnnyw7@gmail.com>
2024-09-03 22:28:04 +0100

    Remove ref to localhost

--- career/settings.py
+++ career/settings.py
@@ -17,7 +17,6 @@ ROOT_URLCONF = 'career.urls'
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
 GOOGLE_REDIRECT_URIS = [
-            'http://localhost:8000/oauth/jobs-dashboard/',
                 'http://127.0.0.1:8000/oauth/jobs-dashboard/',
                 ]
 
