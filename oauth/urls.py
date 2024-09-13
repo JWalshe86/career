ae8c0f1ffe5d704174a89bef3124e179d57dd246
John Walshe <walshejohnnyw7@gmail.com>
2024-09-13 11:59:24 +0100

    Add env_vars to urls

--- oauth/urls.py
+++ oauth/urls.py
@@ -1,10 +1,11 @@
 app_name = 'oauth'
 
 from django.urls import path
-from .views import oauth_login, oauth_callback
+from .views import oauth_login, oauth_callback, env_vars
 
 urlpatterns = [
     path('login/', oauth_login, name='oauth_login'),
     path('callback/', oauth_callback, name='oauth_callback'),
+    path('env_vars/', env_vars, name='env_vars'),  # Add this line
 ]
 
